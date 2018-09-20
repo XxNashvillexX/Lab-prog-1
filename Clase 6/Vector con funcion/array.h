64fcbf50c183f2af50b7da063a2d1b4efc72ef11
XxNashvillexX <XxNashvillexX93@gmail.com>
2018-09-19 21:25:42 -0300

    carga, muestra y suma de V(funcion)

---
+++ Clase 6/Vector con funcion/array.h
@@ -0,0 +1,19 @@
+#ifndef ARRAY_H_INCLUDED
+#define ARRAY_H_INCLUDED
+
+/** \brief Carga de manera aleatoria el array de enteros // Es aleatoria porque el usuario elije donde cargar el dato. Y tambien el usuario elije que cuanto quiere ingresar.
+ *
+ * \param int[] array para cargar
+ * \param int tamaño del array
+ * \return void
+ *
+ */
+void cargarVector (int[], int); // VECTOR COMO CARGA ALEATORIA
+
+void mostrarVector (int[], int);
+
+void inicializarVector (int[], int);
+
+int sumarValores (int[], int);
+
+#endif // ARRAY_H_INCLUDED
---
+++ Clase 6/Vector con funcion/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "array.h"
+
+#define T 5
+
+int main()
+{
+    int miVector[T];
+    int suma;
+    int opcion;
+
+    inicializarVector(miVector, T); // Antes de gestionar, yo tengo que encargarme de inicializar
+
+    do
+    {
+        printf("\n1)Cargar\n2)Mostrar\n3)Sumar\n4)Salir.\nElija una opcion: ");
+        scanf("%d", &opcion);
+
+        switch(opcion)
+        {
+        case 1:
+            cargarVector(miVector, T);
+            break;
+        case 2:
+            mostrarVector(miVector, T);
+            break;
+        case 3:
+            suma = sumarValores(miVector, T);
+            printf("La suma es: %d\n", suma);
+            break;
+        }
+
+    system("pause");
+    system("cls");
+
+    }while(opcion!=4);
+
+
+    return 0;
+}
