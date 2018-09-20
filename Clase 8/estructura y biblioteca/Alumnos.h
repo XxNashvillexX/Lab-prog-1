c621b6828f370b1109130d98eb29f7e6367e80d2
XxNashvillexX <XxNashvillexX93@gmail.com>
2018-09-19 21:59:35 -0300

    Estructura usando biblioteca

---
+++ Clase 8/estructura y biblioteca/Alumnos.h
@@ -0,0 +1,17 @@
+#ifndef ALUMNOS_H_INCLUDED
+#define ALUMNOS_H_INCLUDED
+
+typedef struct{
+int legajo;
+char nombre[50]; // Guarda una cadena. Por eso es un vector.
+char direccion[50];
+float promedio;
+}eAlumno;
+
+
+eAlumno cargarAlumno();
+void mostrarAlumno (eAlumno alumnoParametro);
+void cargarListado(eAlumno[], int tam);
+void mostrarListado(eAlumno[], int tam);
+
+#endif // ALUMNOS_H_INCLUDED
